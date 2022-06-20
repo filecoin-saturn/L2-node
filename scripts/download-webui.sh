c43d2b4a806a3f156435a681fb606bf59262c60b
Miroslav Bajtoš <saturn@bajtos.net>
2022-06-20 13:00:35 +0200

    chore: fix download-webui script
    - Make the file executable
    - Run `mkdir -p` to ensure the target directory exists
    - Fix the output directory resolve the path against the script file,
      not against the current working directory.
    
    Signed-off-by: Miroslav Bajtoš <saturn@bajtos.net>

--- scripts/download-webui.sh
+++ scripts/download-webui.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+OUTDIR=$(dirname $0)/../resources/webui/
+
+echo "⇣ Downloading webui dist to $OUTDIR ⇣"
+
+mkdir -p $OUTDIR
+
 # Downloads latest release
 url=$(curl -s https://api.github.com/repos/filecoin-project/saturn-l2-webui/releases/latest | \
     jq -r '.assets[] | select(.name|match("saturn-l2-webui.tar.gz$")) | .browser_download_url')
 
-curl -L $url | tar -zx -C ../resources/webui/
+curl -L $url | tar -zx -C $OUTDIR
