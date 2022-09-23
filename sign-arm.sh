abf2b9fa4628d6fa1244b50f2d84c1c482c375dc
Julian Gruber <julian@juliangruber.com>
2022-09-23 10:31:35 +0200

    fix paths

--- sign-arm.sh
+++ sign-arm.sh
@@ -4,13 +4,13 @@ set -e
 codesign --force \
   --timestamp \
   --options runtime \
-  --sign "Developer ID Application: Protocol Labs, Inc." dist/macos-x86-64_darwin_amd64_v1/L2-node
+  --sign "Developer ID Application: Protocol Labs, Inc." dist/macos-arm64_darwin_arm64/L2-node
 
-zip -r dist/L2-node_Darwin_x86_64.zip dist/macos-x86-64_darwin_amd64_v1/L2-node
+zip -r dist/L2-node_Darwin_arm64.zip dist/macos-arm64_darwin_arm64/L2-node
 
 xcrun altool --notarize-app \
    --primary-bundle-id "saturn.filecoin.l2-node" \
    -u "oli@protocol.ai" \
    -p "${AC_PASSWORD}" \
    -t osx \
-   -f dist/L2-node_Darwin_x86_64.zip
+   -f dist/L2-node_Darwin_arm64.zip
