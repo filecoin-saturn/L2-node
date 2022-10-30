ae20da91337738342d4b243f11fea8d15d926eca
Christopher Baek <christopher.baek@gmail.com>
2022-10-29 18:20:18 -0700

    Update download-webui.sh script to follow redirects

--- scripts/download-webui.sh
+++ scripts/download-webui.sh
@@ -7,7 +7,7 @@ echo "⇣ Downloading webui dist to $OUTDIR ⇣"
 mkdir -p $OUTDIR
 
 # Downloads latest release
-url=$(curl -s https://api.github.com/repos/filecoin-saturn/node-webui/releases/latest | \
+url=$(curl -L -s https://api.github.com/repos/filecoin-saturn/node-webui/releases/latest | \
     jq -r '.assets[] | select(.name|match("saturn-webui.tar.gz$")) | .browser_download_url')
 
 curl -L $url | tar -zx -C $OUTDIR
